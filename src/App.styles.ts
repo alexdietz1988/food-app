59fad766834693aee148f5e098866cf776ccf9a6
Alex Dietz <adietz@thehumaneleague.org>
2025-07-13 14:50:00 -0400

    Remove Meals and simplify

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -10,3 +10,21 @@ export const H1 = styled.h1`
   font-size: 1.5rem;
   margin-block-end: 1rem;
 `;
+
+export const FiltersContainer = styled.div`
+  padding-block: 1rem;
+
+  & > div {
+    margin-bottom: 0.5rem;
+  }
+`;
+
+export const Filter = styled.div`
+  display: flex;
+  gap: 1rem;
+`;
+
+export const FilterLabel = styled.div<{ selected: boolean }>`
+  cursor: pointer;
+  font-weight: ${(props) => (props.selected ? '600' : '300')};
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { useState } from 'react';
 import * as Styled from './App.styles';
-import { type Food, type Filter } from './types';
+import { type Filter, seasons, nutrients } from './types';
 import { foods as foodsRaw } from './data';
+import FoodList from './FoodList/FoodList';
 
-import FoodIdeas from './FoodIdeas/FoodIdeas';
-import Meals from './Meals/Meals';
+const App = () => {
+  const [filter, setFilter] = useState<Filter>({
+    season: 'all',
+    nutrient: 'all',
+  });
 
-const getFilteredFoods = (foods: Food[], filter: Filter) =>
-  foods.filter((food) => {
+  const foods = foodsRaw.filter((food) => {
     const matchesSeason =
       filter.season === 'all' ||
       (food.seasons && food.seasons.includes(filter.season));
@@ -16,17 +19,36 @@ const getFilteredFoods = (foods: Food[], filter: Filter) =>
     return matchesSeason && matchesNutrient;
   });
 
-const App = () => {
-  const [filter, setFilter] = useState<Filter>({
-    season: 'all',
-    nutrient: 'all',
-  });
-  const foods = getFilteredFoods(foodsRaw, filter);
-
   return (
     <Styled.Container>
-      <FoodIdeas filter={filter} setFilter={setFilter} foods={foods} />
-      <Meals foods={foods} />
+      <div>
+        <Styled.H1>Food Ideas</Styled.H1>
+        <Styled.FiltersContainer>
+          <Styled.Filter>
+            <h2>Seasons:</h2>
+            {seasons.map((season) => (
+              <Styled.FilterLabel
+                onClick={() => setFilter((prev) => ({ ...prev, season }))}
+                selected={filter.season === season}
+              >
+                {season}
+              </Styled.FilterLabel>
+            ))}
+          </Styled.Filter>
+          <Styled.Filter>
+            <h2>Nutrients:</h2>
+            {nutrients.map((nutrient) => (
+              <Styled.FilterLabel
+                onClick={() => setFilter((prev) => ({ ...prev, nutrient }))}
+                selected={filter.nutrient === nutrient}
+              >
+                {nutrient}
+              </Styled.FilterLabel>
+            ))}
+          </Styled.Filter>
+        </Styled.FiltersContainer>
+        <FoodList foods={foods} />
+      </div>
     </Styled.Container>
   );
 };
